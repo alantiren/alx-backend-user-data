dc3b96f00410ae3b5688e424262e2829c19627a5
alantiren <tirenalan0@gmail.com>
2024-01-25 12:29:58 +0300

    Create user.py

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""The `user` model's module.
+
+This module defines the `User` model, representing a record from the `users` table.
+
+Attributes:
+    Base: A declarative base from SQLAlchemy for the model.
+    User (class): Represents a record from the `users` table.
+
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Declarative base for the model
+Base = declarative_base()
+
+class User(Base):
+    """Represents a record from the `users` table.
+
+    Attributes:
+        id (int): The primary key of the user record.
+        email (str): The email of the user (unique).
+        hashed_password (str): The hashed password of the user.
+        session_id (str): The session ID associated with the user.
+        reset_token (str): The reset token used for password reset.
+
+    """
+
+    # Table name in the database
+    __tablename__ = "users"
+
+    # Columns in the table
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False, unique=True)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
