9951ade818feb085a9acb39151e32a3eb3946393
alantiren <tirenalan0@gmail.com>
2024-01-25 13:15:38 +0300

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,24 +2,14 @@
 """
 DB module for handling database operations.
 """
-from sqlalchemy import create_engine, Column, Integer, String, MetaData
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
 
-
-Base = declarative_base()
-
-
-class User(Base):
-    """
-    User class representing the 'users' table.
-    """
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, nullable=False)
-    email = Column(String(256), nullable=False, unique=True)
-    hashed_password = Column(String(60), nullable=False)
-    session_id = Column(String(36), nullable=True)
-    reset_token = Column(String(36), nullable=True)
+from user import Base, User
 
 
 class DB:
@@ -56,10 +46,14 @@ class DB:
         Returns:
             User: The added User object.
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-        return user
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
+        return new_user
 
     def find_user_by(self, **kwargs) -> User:
         """
@@ -74,8 +68,19 @@ class DB:
         Raises:
             NoResultFound: If no user is found.
         """
-        user = self._session.query(User).filter_by(**kwargs).one()
-        return user
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """
@@ -90,9 +95,16 @@ class DB:
             ValueError: If an invalid argument is provided.
         """
         user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_source = {}
         for key, value in kwargs.items():
             if hasattr(User, key):
-                setattr(user, key, value)
+                update_source[getattr(User, key)] = value
             else:
-                raise ValueError(f"Invalid argument: {key}")
+                raise ValueError()
+        self._session.query(User).filter(User.id == user_id).update(
+            update_source,
+            synchronize_session=False,
+        )
         self._session.commit()
